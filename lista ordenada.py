0b1ccf8a44d1d9443e9a787b443e6fad70bde7c4
Kevilicious <utm24090280@utma.edu.mx>
2025-09-29 01:23:00 -0600

    limpiando cagadero

--- lista ordenada.py
+++
@@ -1,8 +0,0 @@
-lista = [2,3,1,6,4,10,9,5,8,7]
-
-for i in range(len(lista)):
-    for j in range(0, len(lista) - i - 1):
-        if lista[j] > lista[j + 1]:
-            lista[j], lista[j + 1] = lista[j + 1], lista[j]
-
-print(lista)
